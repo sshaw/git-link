37f30c3d6b67362f5d7e23ef58c793d19f0c9137
sshaw <skye.shaw@gmail.com>
2013-11-22 00:26:03 -0500

    rename functions using new repo's name

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-git-link
-========
+# git-link
 
-Emacs function to create a URL representing the current buffer's location in its GitHub/Bitbucket/... repository at
-the current line number or active region. The URL will be added to the kill ring.
- 
+An interactive Emacs function that creates a URL representing the current buffer's location
+in its GitHub/Bitbucket/... repository at the current line number or active region.
+The resulting URL will be added to the kill ring.
+
+## Usage
+
+`M-x git-link`
+
 With a prefix argument prompt for the remote's name. Defaults to "origin".
+
--- git-link.el
+++ git-link.el
@@ -1,7 +1,7 @@
-;;; gh-link.el --- Create URLs to a buffer's location in its GitHub repository
+;;; git-link.el --- Create URLs to a buffer's location in its GitHub repository
 
 ;; Author: Skye Shaw <skye.shaw@gmail.com>
-;; Version: 0.0.1
+;; Version: 0.0.2
 
 ;;; Commentary:
 
@@ -10,31 +10,37 @@
 ;; 
 ;; With a prefix argument prompt for the remote's name. Defaults to "origin".
 
-(defvar gh-link-default-remote "origin" "Name of the remote branch to link to")
+(defvar git-link-default-remote "origin" "Name of the remote branch to link to")
 
-(defun gh-link-chomp (s)
+(defun git-link-chomp (s)
   (if (string-match "\\(\r?\n\\)+$" s)
       (replace-match "" t t s)
     s))
 
-(defun gh-link-exec (cmd)
+(defun git-link-exec (cmd)
   (shell-command-to-string (format "%s 2>%s" cmd null-device)))
 
-(defun gh-link-repo-root ()
-  (gh-link-chomp (gh-link-exec "git rev-parse --show-toplevel")))
+(defun git-link-repo-root ()
+  (git-link-chomp (git-link-exec "git rev-parse --show-toplevel")))
 
-(defun gh-link-branch ()
-  (let ((branch (gh-link-exec "git symbolic-ref HEAD")))
-      (if (string-match "/\\(\\w+\\)$" branch)
+(defun git-link-branch ()
+  (let ((branch (git-link-exec "git symbolic-ref HEAD")))
+      (if (string-match "/\\([^/]+?\\)$" branch)
 	  (match-string 1 branch))))
 
-(defun gh-link-remote-dir (remote-name)
-  (let ((remote-dir (gh-link-exec (format "git config --get remote.%s.url" remote-name))))
+(defun git-link-remote-dir (remote-name)
+  (let ((remote-dir (git-link-exec (format "git config --get remote.%s.url" remote-name))))
     (if (and remote-dir
-	     (string-match "\\([-.[:word:]]+/[-.[:word:]]+\\)\\(?:\\.git\\)$" remote-dir))
-	(match-string 1 remote-dir))))
+	     ;; matches traditional URL and scp style
+	     (string-match "\\([-.[:word:]]+\\)[:/]\\([^/]+/[^/]+?\\)\\(?:\\.git\\)?$" remote-dir))
+	;; (match-string 1 remote) ;; host
+	;; (match-string 2 remote) ;; user/repo
+	(match-string 2 remote-dir))))
 
-(defun gh-build-link (dir branch filename)
+(git-link-remote-dir "origin")
+
+
+(defun git-link-build-link (dir branch filename)
   (format "http://github.com/%s/tree/%s/%s#%s"
 	  dir
 	  branch
@@ -44,19 +50,19 @@
 		     (mapcar 'line-number-at-pos (list (region-beginning) (region-end))))
 	    (format "L%s" (line-number-at-pos)))))
 
-(defun gh-link (&optional prompt?)
+(defun git-link (&optional prompt?)
   "Create a URL representing the current buffer's location in its GitHub repository at
  the current line number or active region. The URL will be added to the kill ring.
  
  With a prefix argument prompt for the remote's name. Defaults to \"origin\"."
 
   (interactive "P")
-  (let* ((local-root  (gh-link-repo-root))
+  (let* ((local-root  (git-link-repo-root))
 	 (filename    (buffer-file-name))
-	 (remote-name (if prompt? (read-string "Remote: " nil nil gh-link-default-remote)
-			gh-link-default-remote))
-	 (remote-root (gh-link-remote-dir remote-name))
-	 (branch      (gh-link-branch)))
+	 (remote-name (if prompt? (read-string "Remote: " nil nil git-link-default-remote)
+			git-link-default-remote))
+	 (remote-root (git-link-remote-dir remote-name))
+	 (branch      (git-link-branch)))
 
 
     (cond ((null filename)
@@ -65,6 +71,6 @@
 	   (message "Detached head"))
 	  ((null remote-root)
 	   (message "Cannot deternmine remote root directory"))
-	  ((kill-new (gh-build-link remote-root
-				    branch
-				    (substring filename (1+ (length local-root)))))))))
+	  ((kill-new (git-link-build-link remote-root
+					  branch
+					  (substring filename (1+ (length local-root)))))))))
