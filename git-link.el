aa8462081b95bfae0b81b466a191dcf6c31a8a12
sshaw <skye.shaw@gmail.com>
2014-02-22 11:55:17 -0500

    use transient-mark-mode and mark-active for older versions

--- git-link.el
+++ git-link.el
@@ -54,7 +54,7 @@
 (defun git-link-current-branch ()
   (let ((branch (git-link-exec "git symbolic-ref HEAD")))
       (if (string-match "/\\([^/]+?\\)$" branch)
-          (match-string 1 branch))))
+	  (match-string 1 branch))))
 
 (defun git-link-repo-root ()
   (git-link-chomp (git-link-exec "git rev-parse --show-toplevel")))
@@ -64,20 +64,20 @@
 
 (defun git-link-relative-filename ()
   (let* ((filename (buffer-file-name))
-         (dir      (git-link-repo-root)))
+	 (dir      (git-link-repo-root)))
     (if (and dir filename)
-        (substring (file-truename filename)
+	(substring (file-truename filename)
 		   (1+ (length dir))))))
 
 (defun git-link-remote-host (remote-name)
   (let ((url (git-link-remote-url remote-name)))
     (if (string-match git-link-remote-regex url)
-        (match-string 1 url))))
+	(match-string 1 url))))
 
 (defun git-link-remote-dir (remote-name)
   (let ((url (git-link-remote-url remote-name)))
     (if (string-match git-link-remote-regex url)
-        (match-string 2 url))))
+	(match-string 2 url))))
 
 (defun git-link-github (hostname dirname filename branch commit start end)
   (format "https://github.com/%s/tree/%s/%s#%s"
@@ -104,42 +104,42 @@
 	  start))
 
 (defun git-link (&optional prompt?)
-  "Create a URL representing the current buffer's location in its GitHub/Bitbucket/Gitorious/... 
+  "Create a URL representing the current buffer's location in its GitHub/Bitbucket/Gitorious/...
 repository at  the current line number or active region. The URL will be added to the kill ring.
 
  With a prefix argument prompt for the remote's name. Defaults to \"origin\"."
 
   (interactive "P")
   (let* ((remote-name (if prompt? (read-string "Remote: " nil nil git-link-default-remote)
-                        git-link-default-remote))
-         (remote-host (git-link-remote-host remote-name))
-         (filename    (git-link-relative-filename))
-         (branch      (git-link-current-branch))
-         (commit      (git-link-last-commit))
-         (handler     (nth 1 (assoc remote-host git-link-remote-alist)))
-         (lines       (if (region-active-p)            ;; instead of mark-active?
+			git-link-default-remote))
+	 (remote-host (git-link-remote-host remote-name))
+	 (filename    (git-link-relative-filename))
+	 (branch      (git-link-current-branch))
+	 (commit      (git-link-last-commit))
+	 (handler     (nth 1 (assoc remote-host git-link-remote-alist)))
+	 (lines       (if (and transient-mark-mode mark-active)           ;; change to use-region-p (>= 23)
 			  (mapcar 'line-number-at-pos (list (region-beginning) (region-end)))
-                        (list (line-number-at-pos)))))
+			(list (line-number-at-pos)))))
 
     (cond ((null filename)
-           (message "Buffer has no file"))
-          ((null remote-host)
-           (message "Unknown remote '%s'" remote-name))
-          ((and (null commit) (null branch))
-           (message "Not on a branch, and repo does not have commits"))
-          ;; functionp???
-          ((null handler)
-           (message "No handler for %s" remote-host))
-          ;; null ret val
-          ((kill-new
-            (funcall handler
-                     remote-host
+	   (message "Buffer has no file"))
+	  ((null remote-host)
+	   (message "Unknown remote '%s'" remote-name))
+	  ((and (null commit) (null branch))
+	   (message "Not on a branch, and repo does not have commits"))
+	  ;; functionp???
+	  ((null handler)
+	   (message "No handler for %s" remote-host))
+	  ;; null ret val
+	  ((kill-new
+	    (funcall handler
+		     remote-host
 		     (git-link-remote-dir remote-name)
-                     filename
-                     branch
-                     commit
-                     (nth 0 lines)
-                     (nth 1 lines)))))))
+		     filename
+		     branch
+		     commit
+		     (nth 0 lines)
+		     (nth 1 lines)))))))
 
 (provide 'git-link)
 ;;; --- git-link.el ends here
