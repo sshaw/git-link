27d642347e3d687a7a2214dbc53d1526e98f7571
sshaw <skye.shaw@gmail.com>
2018-06-07 00:39:12 -0400

    Update v0.7.0 release date

--- git-link.el
+++ git-link.el
@@ -35,7 +35,7 @@
 
 ;;; Change Log:
 
-;; 2018-06-03 - v0.7.0
+;; 2018-06-07 - v0.7.0
 ;; * Add support for Tramp (Issue #49, thanks Jürgen Hötzel)
 ;; * Fix various compiler warnings
 ;; * Fix differences between url-path-and-query across Emacs versions
